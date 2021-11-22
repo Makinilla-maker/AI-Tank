21b79ef2aa15b4b8073c7b858202504cb630fbe2
Makinilla-maker <60987205+Makinilla-maker@users.noreply.github.com>
2021-11-22 19:11:03 +0100

    They look each other
    Co-Authored-By: Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>

--- Tank-project/Assets/_Completed-Assets/Mineprefab/LookAtTheEnemy.cs
+++ Tank-project/Assets/_Completed-Assets/Mineprefab/LookAtTheEnemy.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class LookAtTheEnemy : MonoBehaviour
 {
+    public static LookAtTheEnemy instance;
     public Transform enemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(enemy);
+        Vector3 v = new Vector3(enemy.transform.position.x,transform.position.y,enemy.transform.position.z);
+        transform.LookAt(v);
     }
 }
--- Tank-project/Assets/_Completed-Assets/Mineprefab/Shoot.cs
+++ Tank-project/Assets/_Completed-Assets/Mineprefab/Shoot.cs
@@ -4,28 +4,61 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    [SerializeField] private Transform redTank;
-    [SerializeField] private Transform blueTank;
     [SerializeField] private Transform blueCannon;
     [SerializeField] private Transform redCannon;
+    
+    public Rigidbody m_Shell;   
+
     public float range;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        redCannon = GameObject.FindGameObjectWithTag("RedTank").transform;
-        blueCannon = GameObject.FindGameObjectWithTag("BlueTank").transform;
+        redCannon = GameObject.FindGameObjectWithTag("RedCannon").transform;
+        blueCannon = GameObject.FindGameObjectWithTag("BlueCannon").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        if(LookAtTheEnemy.instance.enemy.gameObject.tag == "RedTank")
+        
+        if(gameObject.tag == "RedCannon")
         {
-            if(Physics.Raycast(firePoint.transform.position, firePoint.transform.forward, out hit, range))
+            RaycastHit hit;
+            if(Physics.Raycast(redCannon.position, redCannon.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.tag);
+
+                if(hit.transform.tag == "BlueTank" || hit.transform.tag == "BlueCannon")
+                {
+                    Debug.DrawLine(gameObject.transform.position,hit.transform.position,Color.red);
+                    ShootBullet();
+                }
+            }
         
         }
+        else
+        {
+            RaycastHit hit2;
+            if(Physics.Raycast(blueCannon.position, blueCannon.forward, out hit2, range))
+            {
+                Debug.Log(hit2.transform.tag);
+
+                if(hit2.transform.tag == "RedTank" || hit2.transform.tag == "RedCannon")
+                {
+                    Debug.DrawLine(gameObject.transform.position,hit2.transform.position,Color.blue);
+                    ShootBullet();
+                }
+            }
+        }
+    }
+    void ShootBullet()
+    {
+         Rigidbody shellInstance =
+                Instantiate (m_Shell, gameObject.transform.position, gameObject.transform.rotation) as Rigidbody;
+
+            // Set the shell's velocity to the launch force in the fire position's forward direction.
+            //shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; 
     }
 }
