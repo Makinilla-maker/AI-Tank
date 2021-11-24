37918578e14f0db903537dd697114f8599cacfaa
Makinilla-maker <60987205+Makinilla-maker@users.noreply.github.com>
2021-11-25 00:12:43 +0100

    Patrol Implemented

--- Tank-project/Assets/_Completed-Assets/Scripts/Behaviour/BlueTank/WanderBlue.cs
+++ Tank-project/Assets/_Completed-Assets/Scripts/Behaviour/BlueTank/WanderBlue.cs
@@ -4,34 +4,32 @@ using UnityEngine;
 
 namespace BBUnity.Actions
 {
-    /// <summary>
-    /// It is an action to move towards the given goal using a NavMeshAgent.
-    /// </summary>
-    [Action("Wander Blue")]
+     [Action("Wander Blue")]
     [Help("Moves with wander")]
     public class WanderBlue : GOAction
     {
-        ///<value>Input target game object towards this game object will be moved Parameter.</value>
-        [InParam("target")]
-        [Help("Target game object towards this game object will be moved")]
-        public GameObject target;
+        
 
         [InParam("agent")]
         [Help("agent")]
         private UnityEngine.AI.NavMeshAgent agent;
 
-        [InParam("floor")]
-        [Help("floor")]
-        public Collider floor;
+        [InParam("destPoint")]
+        [Help("destPoint")]
+        private int destPoint = 0;
 
-        /// <summary>Initialization Method of MoveToGameObject.</summary>
-        /// <remarks>Check if GameObject object exists and NavMeshAgent, if there is no NavMeshAgent, the default one is added.</remarks>
+        public GameObject[] waypoints = new GameObject[10];
+
         public override void OnStart()
         {
-            if (target == null)
+            if (GameObject.Find("PatrolList").GetComponent<PatrolList>().patrolList == null)
             {
                 Debug.LogError("The movement target of this game object is null", gameObject);
                 return;
+            }
+            for(int i = 0; i< GameObject.Find("PatrolList").GetComponent<PatrolList>().patrolList.Length; ++i)
+            {
+                waypoints[i] = GameObject.Find("PatrolList").GetComponent<PatrolList>().patrolList[i];
             }
 
             agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
@@ -40,7 +38,8 @@ namespace BBUnity.Actions
                 Debug.LogWarning("The " + gameObject.name + " game object does not have a Nav Mesh Agent component to navigate. One with default values has been added", gameObject);
                 agent = gameObject.AddComponent<UnityEngine.AI.NavMeshAgent>();
             }
-			agent.SetDestination(target.transform.position);
+			agent.autoBraking = false;
+
             
             #if UNITY_5_6_OR_NEWER
                 agent.isStopped = false;
@@ -49,36 +48,23 @@ namespace BBUnity.Actions
             #endif
         }
 
-        /// <summary>Method of Update of MoveToGameObject.</summary>
-        /// <remarks>Verify the status of the task, if there is no objective fails, if it has traveled the road or is near the goal it is completed
-        /// y, the task is running, if it is still moving to the target.</remarks>
-        Vector3 wanderTarget = Vector3.zero;
+        void GotoNextPoint() {
+                       
+            agent.destination = waypoints[destPoint].transform.position;
+            destPoint = (destPoint + 1) % waypoints.Length;
+            //Debug.Log(GameObject.Find("PatrolList").GetComponent<PatrolList>().patrolList[destPoint].position);
+        }
         public override TaskStatus OnUpdate()
         {
-            //Debug.Log("wanderiiiiiiiiiiiiiiiing");
-            float wanderRadius = 10;
-            float wanderDistance = 10;
-            float wanderJitter = 1;
-                wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter,
-                                            0,
-                                            Random.Range(-1.0f, 1.0f) * wanderJitter);
-            wanderTarget.Normalize();
-            wanderTarget *= wanderRadius;
-
-            Vector3 targetLocal = wanderTarget + new Vector3(0, 0, wanderDistance);
-            Vector3 targetWorld = this.gameObject.transform.InverseTransformVector(targetLocal);
-
-            if (!floor.bounds.Contains(targetWorld))
+            Debug.Log(waypoints.Length);
+            Debug.Log(waypoints[0].transform.position);
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
             {
-                targetWorld = -agent.transform.position * 0.1f;
-            };
+                GotoNextPoint();
+            }
 
-            agent.SetDestination(targetWorld);
-            
             return TaskStatus.RUNNING;
         }
-        /// <summary>Abort method of MoveToGameObject </summary>
-        /// <remarks>When the task is aborted, it stops the navAgentMesh.</remarks>
         public override void OnAbort()
         {
 
